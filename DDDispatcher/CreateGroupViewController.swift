b8f7b59f4a24e58e0e61dd11e582cec18d605b45
Yoonju Lee <yoonjulee@Yoonjuui-MacBook-Pro.local>
2017-04-02 19:57:54 -0400

    accessing photo library works. but selecting image is not working. It looks like a bug in Xcode

--- DDDispatcher/CreateGroupViewController.swift
+++ DDDispatcher/CreateGroupViewController.swift
@@ -14,18 +14,26 @@ import UIKit
 import Firebase
 import FirebaseDatabase
 
-class CreateGroupViewController: UIViewController, UITextViewDelegate {
+class CreateGroupViewController: UIViewController, UITextViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    let picker = UIImagePickerController()
+
     @IBOutlet weak var avatarButton: UIButton!
     @IBOutlet weak var createGroupButton: UIButton!
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var groupNameTextField: UITextField!
-
+    @IBOutlet weak var groupLogo: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         setupUI()
+        
     }
+  /*
+    override func viewDidLayoutSubviews() {
+        setupUI()
+    }*/
     
     func setupUI() {
         setupButton()
@@ -33,6 +41,7 @@ class CreateGroupViewController: UIViewController, UITextViewDelegate {
     }
     
     func setupButton() {
+     
         createGroupButton.layer.cornerRadius = 15
         avatarButton.layer.borderWidth = 1
         avatarButton.layer.masksToBounds = false
@@ -40,6 +49,7 @@ class CreateGroupViewController: UIViewController, UITextViewDelegate {
         avatarButton.layer.borderColor = UIColor.white.cgColor
         avatarButton.layer.cornerRadius = avatarButton.frame.height/2
         avatarButton.clipsToBounds = true
+        avatarButton.contentMode = .scaleAspectFit
     }
     
     func setupTextView() {
@@ -115,20 +125,49 @@ class CreateGroupViewController: UIViewController, UITextViewDelegate {
         }
     }
     
-    @IBAction func selectAvatar(_ sender: Any) {
-        //Ask the user to select a picture
-        //CACHE: store user info here
-        
-    }
-    
 
     @IBAction func sendBack(_ sender: Any) {
         self.performSegue(withIdentifier: "unwindMenuSegue", sender: self)
     }
     
+    //**********************************
+    //MARK: Upload Image
+    //**********************************
+    
+    @IBAction func selectAvatar(_ sender: Any) {
+        print("selectAvatar\n")
+        picker.allowsEditing = true
+        picker.sourceType = .photoLibrary
+        picker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        present(picker, animated:true, completion:nil)
+    }
 
-    
-    
-    
+    //get out of the library
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print("dismiss image picker\n")
+        dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+       // avatarButton.contentMode = .scaleAspectFit
+        self.dismiss(animated: true, completion: nil)
+
+        
+        if let selectedImage = info[UIImagePickerControllerEditedImage] as? UIImage {
+           // avatarButton.setImage(selectedImage, for: UIControlState.normal)
+            avatarButton.imageView?.image = selectedImage
+
+print("111")
+        }else if let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            //avatarButton.setImage(selectedImage, for: UIControlState.normal)
+            avatarButton.imageView?.image = selectedImage
+print("222")
+        }else {
+            print("somethings wrong")
+        }
+
+       // self.dismiss(animated: true, completion: nil)
+    }
 }
 
